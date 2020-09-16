3952f08967b43e180665a4c61beb9e0dcaf65906
jcook3195 <jcook3195@gmail.com>
2020-09-16 09:40:16 -0400

    initial commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+  } catch (err) {
+    // exit with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
