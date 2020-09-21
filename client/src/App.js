a1b3eaa9a04269b683d791df77673026404143bf
jcook3195 <jcook3195@gmail.com>
2020-09-20 22:22:57 -0400

    added basic redux functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './store/actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <h1>App</h1>
-    </Fragment>
-  </Router>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <h1>App</h1>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
---
+++ client/src/store/actions/types.js
@@ -0,0 +1,16 @@
+// register user
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+// log in / out
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+
+// user / auth
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+
+// toasts
+export const SET_TOAST = 'SET_TOAST';
+export const REMOVE_TOAST = 'REMOVE_TOAST';
